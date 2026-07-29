d0e6a8f8cc216bf7661cf04481641fc6bc9d69ae
uzair-furqan <avengerneew@gmail.com>
2026-07-29 16:42:41 +0500

    added class five

---
+++ class_5/http_methods.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTTPS METHODS</title>
+</head>
+<body>
+    <form action="profile.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="user_name" id="name">
+        <br>
+        <label for="pass">Password:</label>
+        <input type="password" name="user_password" id="pass">
+        <br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
---
+++ class_5/profile.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<h1>PROFILE</h1>";
+
+$name = $_GET['user_name'];
+$pass = $_GET['user_password'];
+echo $name;
+
+
+?>
