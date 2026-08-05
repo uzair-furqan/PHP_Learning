9b3d89ff38eb41553de769a82a6c0b400b6041ec
uzair-furqan <uzairfurqan30@gmail.com>
2026-08-05 16:30:29 +0500

    form validation added

---
+++ class_7/form.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORM</title>
+</head>
+<body>
+
+<?php
+
+$error_flag = "";
+$username_error = "";
+$userpassword_error = "";
+
+if(isset($_POST['login_submit']))
+{
+    $user_name = $_POST["user_name"];
+    $user_pass = $_POST["user_password"];
+
+    if(empty($user_name))
+    {
+        $error_flag = "yes";
+        $username_error = "PLEASE ENTER THE USERNAME";
+    }
+
+    if(empty($user_pass))
+    {
+        $error_flag = "yes";
+        $userpassword_error = "PLEASE ENTER THE PASSWORD";
+    }
+
+    if(empty($error_flag))
+    {
+        if($user_name == "uzair" && $user_pass == "123")
+            {
+                echo "WElcome $user_name";
+            }
+            else
+            {
+                echo "USERNAME OR PASS WORD IS INCORRECT";
+            }
+    }
+
+}
+
+?>
+
+<h1>LOGIN</h1>
+
+<form action="" method="post">
+
+<label for="u">Username :</label>
+<input type="text" id="u" name="user_name">
+<?php
+if(!empty($username_error))
+    {
+        echo "<p style = 'color:red'>$username_error</p>";
+    }
+?>
+
+<br><br>
+
+
+<label for="p">Password :</label>
+<input type="password" name="user_password" id="p">
+<?php
+if(!empty($userpassword_error))
+    {
+        echo "<p style = 'color:red'>$userpassword_error</p>";
+    }
+?>
+
+
+<br><br>
+
+<input type="submit" value="Login" name="login_submit">
+
+</form>
+
+</body>
+</html>
